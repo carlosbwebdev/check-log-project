2065437cc77b1be1cce33f9acbe34af10432c229
Carlos Barros <carlosbwebdev@gmail.com>
2023-01-20 20:48:20 +0000

    initial commit

---
+++ client/src/components/customHooks/useFetchUsers.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const useFetchUsers = (url) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(url)
+      .then((response) => {
+        setUser(response.data);
+      })
+      .catch((error) => {
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [url]);
+
+  return { user, loading, error };
+};
+
+export default useFetchUsers;
