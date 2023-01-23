9f653ee0ac6ec04f6d6b6b776c9131e665d522b1
Carlos Barros <carlosbwebdev@gmail.com>
2023-01-23 20:26:48 +0000

    initial commit

--- server/schemas/taskSchema.js
+++ server/schemas/taskSchema.js
@@ -1,15 +1,39 @@
 const mongoose = require('mongoose');
 
+// const Task = new mongoose.Schema(
+//   {
+//     title: {
+//       type: String,
+//       required: [true, 'Please provide a title'],
+//     },
+//     description: {
+//       type: String,
+//       required: [true, 'Please provide a description'],
+//     },
+//   },
+//   { collection: 'task-data' }
+//);
 const Task = new mongoose.Schema(
   {
-    title: {
-      type: String,
+    date: {
+      type: Date,
       required: [true, 'Please provide a title'],
     },
-    description: {
-      type: String,
-      required: [true, 'Please provide a description'],
-    },
+    todos: [
+      {
+        title: {
+          type: String,
+          required: [true, 'Please provide a title'],
+        },
+        description: {
+          type: String,
+        },
+        completed: {
+          type: Boolean,
+          default: false,
+        },
+      },
+    ],
   },
   { collection: 'task-data' }
 );
