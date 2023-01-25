ebd9ed49517b77d35f49aeb71db1450525d6acae
Carlos Barros <carlosbwebdev@gmail.com>
2023-01-25 11:00:49 +0000

    delete inline styles and add style module

--- client/src/components/UI/Footer.js
+++ client/src/components/UI/Footer.js
@@ -1,29 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import styles from "../../styles/UI/Footer.module.css";
 
 const Footer = () => {
+  // This state variable is used to store the current time and it is initialized with the current time
+  const [currentTime, setCurrentTime] = React.useState(
+    new Date().toLocaleTimeString("pt-PT")
+  );
+  const currentDate = new Date().toLocaleDateString("pt-PT");
+
+  // This useEffect is used to update the current time every second and set it to the currentTime state variable so it can be displayed in the footer section of the page
+  useEffect(() => {
+    // This interval is used to update the current time every second
+    const interval = setInterval(() => {
+      setCurrentTime(new Date().toLocaleTimeString("pt-PT"));
+    }, 1000);
+    return () => clearInterval(interval); // This is used to clear the interval when the component is unmounted so it doesn't keep updating the current time after the component is unmounted and it doesn't cause any memory leaks or errors in the console of the browser or the terminal of the server
+  }, []);
   return (
-    <div
-      style={{
-        with: "100%",
-        backgroundColor: "rgb(32, 109, 63)",
-        position: "absolute",
-        left: "0",
-        bottom: "0",
-        right: "0",
-        height: "50px",
-        color: "white",
-        textAlign: "center",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        opacity: "0.7",
-        fontSize: "0.8rem",
-      }}
-    >
-      <footer>
-        <span> Made by Carlos Barros</span>
+    <>
+      <footer className={styles.footerSection}>
+        <div className={styles.sectionCenter}>
+          <span> Made by Carlos Barros</span>
+          <span>
+            {currentDate} - {currentTime}
+          </span>
+        </div>
       </footer>
-    </div>
+    </>
   );
 };
 
