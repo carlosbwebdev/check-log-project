3d94edd94835ffbebb227510303fae9c251b50dd
Carlos Barros <carlosbwebdev@gmail.com>
2023-01-25 11:00:59 +0000

    add Routes

--- client/src/index.js
+++ client/src/index.js
@@ -5,14 +5,27 @@ import "./styles/index.css";
 import HomePage from "./pages/HomePage";
 import Dashboard from "./pages/Dashboard";
 import RegisterPage from "./pages/RegisterPage";
+import AddTaksPage from "./pages/adminPages/AddTaksPage";
+import GetTaskTeamPage from "./pages/adminPages/GetTaskTeamPage";
+import Layout from "./components/UI/Layout";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" exact element={<HomePage />} />
+      <Route path="/addtask" exact element={<AddTaksPage />} />
+      <Route path="/seetasksteam" exact element={<GetTaskTeamPage />} />
       <Route path="/register" exact element={<RegisterPage />} />
-      <Route path="/dashboard" exact element={<Dashboard />} />
+      <Route path="/dashboard/user/:userId" exact element={<Dashboard />} />
+      <Route
+        path="*"
+        element={
+          <Layout>
+            <h1>404: Not Found</h1>
+          </Layout>
+        }
+      />
     </Routes>
   </BrowserRouter>
 );
