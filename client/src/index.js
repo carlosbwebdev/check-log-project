7f2b9edcb997021d491b382a78a046011de460d9
Carlos Barros <carlosbwebdev@gmail.com>
2023-02-03 09:05:23 +0000

    add new routes

--- client/src/index.js
+++ client/src/index.js
@@ -6,18 +6,26 @@ import HomePage from "./pages/HomePage";
 import Dashboard from "./pages/Dashboard";
 import RegisterPage from "./pages/RegisterPage";
 import AddTaksPage from "./pages/adminPages/AddTaksPage";
-import GetTaskTeamPage from "./pages/adminPages/GetTaskTeamPage";
+import GetTasksTeamPage from "./pages/adminPages/GetTaskTeamPage";
 import Layout from "./components/UI/Layout";
+import GetTasksUserPage from "./pages/userPages/GetTasksUserPage";
+import ProfilePage from "./pages/ProfilePage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" exact element={<HomePage />} />
-      <Route path="/addtask" exact element={<AddTaksPage />} />
-      <Route path="/seetasksteam" exact element={<GetTaskTeamPage />} />
-      <Route path="/register" exact element={<RegisterPage />} />
       <Route path="/dashboard/user/:userId" exact element={<Dashboard />} />
+      <Route path="/register" exact element={<RegisterPage />} />
+      <Route path="/profile" exact element={<ProfilePage />} />
+
+      {/* //Admin Routes */}
+      <Route path="/addtask" exact element={<AddTaksPage />} />
+      <Route path="/seetasksteam" exact element={<GetTasksTeamPage />} />
+      {/* //Users Routes */}
+      <Route path="/seetasks" exact element={<GetTasksUserPage />} />
       <Route
         path="*"
         element={
