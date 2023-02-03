06226d7aae70aaea0b76f62e0d10b6427769150b
Carlos Barros <carlosbwebdev@gmail.com>
2023-02-03 09:06:37 +0000

    initial commit

---
+++ client/src/components/customHooks/useFetchTaskId.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const useFetchTaskId = (url) => {
+  const [task, setTask] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(url)
+      .then((response) => {
+        setTask(response.data);
+      })
+      .catch((error) => {
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [url]);
+
+  return { task, loading, error };
+};
+
+export default useFetchTaskId;
