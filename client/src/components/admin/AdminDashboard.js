3ee4b7aef59899eb33685b7c174790e220a9393f
Carlos Barros <carlosbwebdev@gmail.com>
2023-02-03 09:06:13 +0000

    add Links to add tasks page and see tasks by team and date

--- client/src/components/admin/AdminDashboard.js
+++ client/src/components/admin/AdminDashboard.js
@@ -1,15 +1,32 @@
 import React from "react";
-import AddTask from "./AddTask";
-import GetAllTaks from "./GetAllTaks";
+import { Link } from "react-router-dom";
+import styles from "../../styles/admin/AdminDashboard.module.css";
 
 const AdminDashboard = (props) => {
   return (
     <div>
-      <h4>Sou Administrador</h4>
-      <AddTask userID={props.userID} />
-
-      {/* //! activar depois */}
-      {/* <GetAllTaks /> */}
+      <section className={styles.dashboardSection}>
+        <h1>Admin Dashboard</h1>
+        <p>A task management application for teams</p>
+        <section className={styles.gridCards}>
+          <Link to="/addtask">
+            <div className={styles.card}>
+              <h3>Create New Daily Tasks</h3>
+              <p>Small Description of what add tasks means...</p>
+            </div>
+          </Link>
+          <div className={styles.card}>
+            <h3>Create New Weekly Tasks</h3>
+            <p>Small Description of what add tasks means...</p>
+          </div>
+          <Link to="/seetasksteam">
+            <div className={styles.card}>
+              <h3>Search Tasks by Date</h3>
+              <p>Small Description of what add tasks means...</p>
+            </div>
+          </Link>
+        </section>
+      </section>
     </div>
   );
 };
